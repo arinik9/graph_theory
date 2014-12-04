a644f1242a1f53c45e59f3e3c526426fe7371e39
gunce <gunceorman@gmail.com>
2014-12-04 16:41:32 +0200

    Update modularity2_matrix_directed.r

--- codes/modularity2_matrix_directed.r
+++ codes/modularity2_matrix_directed.r
@@ -18,3 +18,49 @@ new_sum = new_sum + ((adj_mat[i,j])-(k_i_in*k_j_out)/m)
 return(new_sum/m)
 
 }
+
+
+part=lv$membership
+
+adj_mat=get.adjacency(g, attr="weight")
+
+
+modularity3_directed=function(adj_mat, part){
+
+	
+
+	m=sum(adj_mat)
+
+	v_num=vcount(g)
+
+	
+
+	new_sum=0
+
+
+
+	for ( i in 1:v_num){
+
+		for ( j in 1:v_num){
+
+			if(part[i]==part[j]){
+
+				k_i=sum(E(g)[from(i)]$weight)
+
+				k_j=sum(E(g)[to(j)]$weight)
+
+				new_sum = new_sum + ((adj_mat[i,j]/m)-((k_i*k_j)/(m*m)))
+
+			}
+
+		}
+
+	}
+
+	return(new_sum)
+
+
+
+}
+
+
