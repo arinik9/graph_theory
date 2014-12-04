0ee7df5f360eee9a21842c3987590895744cbc00
arinik9 <arinik9@hotmail.com>
2014-12-04 16:32:13 +0200

    [test edilmedi] louvain algorithm

--- codes/deltaQ.r
+++ codes/deltaQ.r
@@ -30,26 +30,4 @@ deltaQ=function(node, community, graph, weighted){
 
   return term1 - term2
 
-
-
-
-
-
-  sums=0
-  m=sum(E(g)$weight)
-
-  for ( i in coms){
-    my_nodes=which(community_str==i)
-    inner_tot=sum(E(induced.subgraph(g,my_nodes))$weight)
-    outer_tot=sum(E(g)[from(my_nodes)]$weight)-inner_tot
-    eii=(2*inner_tot)/(2*m)
-    aii=((2*inner_tot )+ (outer_tot))/(2*m)
-    sums=sums+(eii-(aii*aii))
-  }
-
-
-
-
-  return(sums)
-
 }
