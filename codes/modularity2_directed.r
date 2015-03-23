d72c12dc6a84976cc23711c526040b4b7810f2a9
Nejat <arinik9@users.noreply.github.com>
2015-03-24 01:00:20 +0200

    modularity function for directed graphs

--- codes/modularity2_directed.r
+++ codes/modularity2_directed.r
@@ -1,93 +1,26 @@
-#modularity for directed graph
-
 modularity2_directed=function(community_str,g,weighted){
-
-  if (weighted){
-    if (get.edge.attribute(g,"weight")[1]){ #get.edge.attribute(g,"weight")[1] => sadece ilk elemanin bilgisi yeterli gibi dusunebiliriz
-    
+    if (weighted){
+       if (which.max(get.edge.attribute(g,"weight"))>1){}
+	else{
+		g=set.edge.attribute(g,"weight",value=1)
+	}
     }
     else{
-      g=set.edge.attribute(g,"weight",value=1)
-    }
-  }
-  else{
-    g=set.edge.attribute(g,"weight",value=1)
-  }
-
-  coms=unique(community_str)
-
-  sums=0
-  m=sum(E(g)$weight)
-
-  for ( i in coms){
-    my_nodes=which(community_str==i)
-    inner_tot=sum(E(induced.subgraph(g,my_nodes))$weight)
-    outer_tot=sum(E(g)[to(my_nodes)]$weight)-inner_tot #adj() ya da from() da kullanabilirdik aslinda ?
-    eii=(inner_tot)/(m)
-    aii=((inner_tot) + (outer_tot))/(m)
-    sums=sums+(eii-(aii*aii))
-  }
-
-
-  return(sums)
-
-}
-
-
-
-odularity2_directed=function(community_str,g,weighted){
-
-
-
-if (weighted){
-
-	if (get.edge.attribute(g,"weight")){}
-
-	else{
-
-		g=set.edge.attribute(g,"weight",value=1)
-
-	}
-
-}
-
-else{
-
 	g=set.edge.attribute(g,"weight",value=1)
-
-}
-
-
+    }	
 
 	coms=unique(community_str)
-
 	sums=0
-
 	m=sum(E(g)$weight)
 
-	for ( i in coms){
-
-		my_nodes=which(community_str==i)
-
-		inside_tot=sum(E(induced.subgraph(g,my_nodes))$weight)
-
-		outer_tot=(sum(E(g)[from(my_nodes)]$weight))
-		inner_tot=(sum(E(g)[to(my_nodes)]$weight))
-		
-
-		eii=(inside_tot)/(m)
-
-		aii=((inner_tot*outer_tot))/(m*m)
-
-		sums=sums+(eii-(aii))
-
-	}
-
-
-
-
-
-	return(sums)
-
+    for ( i in coms){
+	my_nodes=which(community_str==i)
+	inside_tot=sum(E(induced.subgraph(g,my_nodes))$weight)
+	outer_tot=(sum(E(g)[from(my_nodes)]$weight))
+	inner_tot=(sum(E(g)[to(my_nodes)]$weight))
+	eii=(inside_tot)/(m)
+	aii=((inner_tot*outer_tot))/(m*m)
+	sums=sums+(eii-(aii))
+    }
+    return(sums)
 }
-
