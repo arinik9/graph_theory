66d99152eef70c44523806bced5f68f4e9869846
arinik9 <arinik9@hotmail.com>
2015-05-13 14:33:39 +0300

    resolved the problem of integer overflow in directed modularity function

--- louvain/louvain.R
+++ louvain/louvain.R
@@ -66,13 +66,13 @@ modularity_directed=function(g, community_str,weighted){
 
 	  coms=unique(community_str)
 	  sums=0
-	  m=sum(E(g)$weight)
+	  m=as.numeric(sum(E(g)$weight))
 
     for ( i in coms){
 	      my_nodes=which(community_str==i)
-	      inside_tot=sum(E(induced.subgraph(g,my_nodes))$weight)
-	      outer_tot=(sum(E(g)[from(my_nodes)]$weight))
-	      inner_tot=(sum(E(g)[to(my_nodes)]$weight))
+	      inside_tot=as.numeric(sum(E(induced.subgraph(g,my_nodes))$weight))
+	      outer_tot=as.numeric(sum(E(g)[from(my_nodes)]$weight))
+	      inner_tot=as.numeric(sum(E(g)[to(my_nodes)]$weight))
 	      eii=(inside_tot)/(m)
 	      aii=((inner_tot*outer_tot))/(m*m)
 	      sums=sums+(eii-(aii))
